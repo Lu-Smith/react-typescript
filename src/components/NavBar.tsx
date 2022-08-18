91e15c1b9bf53370f6ddbdf08c7ecd575c4326ac
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-18 14:25:01 +0100

    Created pages and NavBar folders

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container, Navbar as NavbarBS} from 'react-bootstrap';
+
+const NavBar: React.FC = () => {
+  return (
+    <NavbarBS>
+        <Container>
+           NavBar 
+        </Container>
+    </NavbarBS>
+  )
+}
+
+export default NavBar;
