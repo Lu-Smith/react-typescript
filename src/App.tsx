db3c1ce8efa9f2baa9b939258e919a8a1cdd224d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-18 14:34:23 +0100

    Connected NavBar to pages
    Removed unnecessary logos
    Add basic bootstrap css to index.html and NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import './App.css';
 import Home from './pages/Home';
 import Store from './pages/Store';
 import About from './pages/About';
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {Container, Navbar as NavbarBS} from 'react-bootstrap';
+import {Container, Nav, Navbar as NavbarBS} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <NavbarBS>
+    <NavbarBS className='bg-white shadow-sm mb-3'>
         <Container>
-           NavBar 
+           <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+           <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+           <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
         </Container>
     </NavbarBS>
   )
