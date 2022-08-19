7f912c5f84c9767412408948a8ba72fe119239c8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-19 18:24:40 +0100

    Updated useLocalStorage.ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,3 +1,15 @@
-export function useLocalStorage() {
+import { useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+    const [value, setValue] = useState<T>(() => {
+        const jsonValue = localStorage.getItem(key)
+        if (jsonValue != null) return JSON.parse(jsonValue)
+
+        if(typeof initialValue === "function") {
+            return (initialValue as () => T)()
+        } else {
+            return initialValue
+        }
+    })
     return "Hello";
 }
