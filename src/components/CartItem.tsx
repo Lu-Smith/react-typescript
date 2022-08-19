e1520ec97e9df238138d21a1c0509f4227210cb8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-19 17:48:05 +0100

    Updated CardItem.tsx

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Stack } from 'react-bootstrap';
 import { useShoppingCart } from '../context/ShoppingCartContext';
 import storeItems from '../data/items.json';
+import { formatCurrency } from '../utilities/formatCurrency';
 
 type CartItemsProps = {
     id: number
@@ -13,8 +14,14 @@ const CartItem = ({id, quantity}: CartItemsProps) => {
     const item = storeItems.find(i => i.id === id)
       if (item == null) return null
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
         <img src={item.imgUrl} style={{ width: "125px", height: "75px", objectFit: "cover"}} />
+        <div className='me-auto'>
+            <div>
+                {item.name} {quantity > 1 && <span style={{fontSize: ".8rem", color: "#cc8214"}}>x{quantity}</span>}
+            </div>
+            <div style={{color: "#cc8214"}}>{formatCurrency(item.price)}</div>
+        </div>
     </Stack>
   )
 }
