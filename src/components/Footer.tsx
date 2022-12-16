1e2b5d673a0bbb79791243547b49a724bbeabe8b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-16 22:45:46 +0000

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,12 +14,15 @@ const Footer: React.FC = () => {
     <div className='Footer'>
          <CardGroup>
       <Card>
-        
         <Card.Body>
-          <Card.Title>Twitter</Card.Title>
-          <Card.Text>
-            Connect with us on our awesome Twitter account. Let sweetness stay with you.
-          </Card.Text>
+            <a  href="//twitter.com/LunaSmithArt"
+                    target="_blank"
+                    aria-label="Instagram">
+                <Card.Title>Twitter</Card.Title>
+                <Card.Text>
+                    Connect with us on our awesome Twitter account. Let sweetness stay with you.
+                </Card.Text>
+            </a>
         </Card.Body>
         <Card.Footer className='Icons'>
         <FaTwitter />
@@ -27,26 +30,52 @@ const Footer: React.FC = () => {
       </Card>
       <Card>
         <Card.Body>
-          <Card.Title>Facebook</Card.Title>
-          <Card.Text>
-            Daily update of candy life{' '}
-          </Card.Text>
+            <a  href="//www.facebook.com/lunasmithart"
+                    target="_blank"
+                    aria-label="Instagram">
+                <Card.Title>Facebook</Card.Title>
+                <Card.Text>
+                    Daily update of candy life{' '}
+                </Card.Text>
+            </a>
         </Card.Body>
         <Card.Footer className='Icons'>
         <FaFacebook />
         </Card.Footer>
       </Card>
+      
       <Card>
         <Card.Body>
-          <Card.Title>LinkedIn</Card.Title>
-          <Card.Text>
-            Share with us you sweet posts. Lets make the sweeter world together.
-          </Card.Text>
+            <a  href="//www.linkedin.com/in/luna-smith/"
+                    target="_blank"
+                    aria-label="Instagram">
+                <Card.Title>LinkedIn</Card.Title>
+                <Card.Text>
+                    Share with us you sweet posts. Lets make the sweeter world together.
+                </Card.Text>
+            </a>
         </Card.Body>
         <Card.Footer className='Icons'>
            <FaLinkedin />
         </Card.Footer>
       </Card>
+      <Card >
+         <Card.Body>
+          <a  href="//www.instagram.com/lunasmithart/"
+                target="_blank"
+                aria-label="Instagram">
+                <Card.Title>Instagram</Card.Title>
+                <Card.Text>
+                    Daily new photos with delicious treats.
+                </Card.Text>
+          </a>
+        </Card.Body>
+        <Card.Footer className='Icons'>
+           <FaInstagram />
+        </Card.Footer>
+        
+      </Card>
+      
     </CardGroup>
 
     </div>
