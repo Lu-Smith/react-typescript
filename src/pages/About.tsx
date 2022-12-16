e68aaaa08aea41b3ac60dbe6a6ae3adea576ba29
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-16 18:30:20 +0000

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 const About: React.FC = () => {
+  const renderTooltip = (props:any) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Yum Yum
+    </Tooltip>
+  )
+
   return (
-    <div>About</div>
+     <div className='About'>
+      <OverlayTrigger
+      placement="right"
+      delay={{ show: 250, hide: 400 }}
+      overlay={renderTooltip}
+      >
+      <span className='Logo'>CandyHeaven</span>
+      </OverlayTrigger>
+    </div>
   )
 }
 
