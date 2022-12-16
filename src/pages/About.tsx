bb9789cfa0953df99723828e2fa70c351e22f457
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-16 18:52:05 +0000

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
+import Accordion from 'react-bootstrap/Accordion';
 
 const About: React.FC = () => {
   const renderTooltip = (props:any) => (
-    <Tooltip id="button-tooltip" {...props}>
-      Yum Yum
+    <Tooltip id="button-tooltip" {...props} >
+      Contact Us
     </Tooltip>
   )
 
@@ -18,6 +19,14 @@ const About: React.FC = () => {
       >
       <span className='Logo'>CandyHeaven</span>
       </OverlayTrigger>
+      <Accordion defaultActiveKey="0" style={{fontSize: "1.2rem"}}>
+          <Accordion.Item eventKey="0" style={{fontSize: "1.2rem", background: "#8214cc", color: "white"}}>
+              <Accordion.Header><span className='ContactUs'>Contact us</span></Accordion.Header>
+              <Accordion.Body>
+                We love to hear from you. Get in touch.
+              </Accordion.Body>
+          </Accordion.Item>
+      </Accordion>
     </div>
   )
 }
